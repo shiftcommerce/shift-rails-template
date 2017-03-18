716083b992d232bc59dfaf358f21347158646605
Ryan Townsend <ryan@ryantownsend.co.uk>
2017-03-18 16:08:31 +0000

    Added Puma config file

--- template.rb
+++ template.rb
@@ -232,6 +232,31 @@ file 'app.json', <<-CODE
 }
 CODE
 
+file 'config/puma.rb', <<-CODE
+# Puma can serve each request in a thread from an internal thread pool.
+# The `threads` method setting takes two numbers a minimum and maximum.
+# Default is 5
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
+threads threads_count, threads_count
+
+# Specifies the `port` that Puma will listen on to receive requests, default is 3000.
+port ENV.fetch("PORT") { 3000 }
+
+# Specifies the `environment` that Puma will run in.
+environment ENV.fetch("RAILS_ENV") { "development" }
+
+# Specifies the number of `workers` to boot in clustered mode.
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+
+preload_app!
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
+
+plugin :tmp_restart
+CODE
+
 file 'Dockerfile', <<-CODE
 FROM ruby:2.4-slim
 LABEL maintainer "ryan@ryantownsend.co.uk"
